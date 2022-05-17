0ba26a3bae4dc4dd553d3bf398423ad25cedc371
kangsoyee <kangse2942@naver.com>
2022-05-17 23:26:54 +0900

    test

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
+const path = require('path');
 
 function createWindow () {
   // Create the browser window.
@@ -22,22 +24,40 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...');
+});
+autoUpdater.on('update-available', (info) => {
+  log.info('업데이트가 가능합니다.');
+});
+autoUpdater.on('update-not-available', (info) => {
+  log.info('현재 최신버전입니다.');
+});
+autoUpdater.on('error', (err) => {
+  log.info('에러가 발생하였습니다. 에러내용 : ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log.info(log_message);
 })
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('업데이트가 완료되었습니다.');
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+/* Electron =====================================================*/
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+/** 초기화가 끝나게 되면 실행 */
+app.on('ready', () => {
+  // 메인 창 생성
+  createWindow();
+
+  // 자동 업데이트 등록
+  autoUpdater.checkForUpdates();
+});
+
+/** [생명주기] 모든 창이 닫히면 자동으로 앱 종료 */
+app.on('window-all-closed', () => {
+  app.quit();
+});
